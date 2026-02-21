8726e0b759b8a9d98a1239f1179e58f07f33b22e
Omar Amairi <oamairi@f6r10s14.clusters.42paris.fr>
2026-02-21 15:43:58 +0100

    first_push

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 12:08:05 by oamairi           #+#    #+#             */
+/*   Updated: 2026/02/21 14:01:51 by oamairi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
+	}
+	int i = 1;
+	while (av[i])
+	{
+		int j = 0;
+		while (av[i] && av[i][j])
+		{
+			if (islower(av[i][j]) != 0)
+				av[i][j] = toupper(av[i][j]);
+			j++;
+		}
+		std::cout << av[i];
+		i++;
+	}
+	std::cout << "\n";
+	return (0);
+}
